56d3dd4b96ac8d5f22e198f7642ee6f14fd661ed
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-07-18 09:42:57 +0300

    fixed errors and added readme

--- components/Bird.jsx
+++ components/Bird.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View,ImageBackground,StyleSheet} from 'react-native';
 
 const Bird = ({birdBottom,birdLeft}) => {
     const birdWidth = 50;
     const birdHeight =60;
     return(
         <View style = {{
-
             position: 'absolute',
-            backgroundColor: 'blue',
             width: birdWidth,
             height: birdHeight,
             bottom: birdBottom - (birdHeight/2),
             left: birdLeft - (birdWidth/2),
-        }}
-        />
-
+        }}>
+        <ImageBackground source=
+        {{ uri: 'https://opengameart.org/sites/default/files/GIF-sample.gif' }}
+        resizeMode="cover" style={styles.image}>
+        </ImageBackground>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    image: {
+      flex: 1,
+      justifyContent: "center",
+      width: 50,
+      height: 60,
+    },
+  });
 
 export default Bird;
